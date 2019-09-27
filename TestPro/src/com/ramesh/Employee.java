37a1287e7e76fdc138c18875049aa2b442d01bfc
Ramesh <chouhanramesh74@gmail.com>
2019-09-27 16:27:06 +0530

    project commit by ramesh

---
+++ TestPro/src/com/ramesh/Employee.java
@@ -0,0 +1,10 @@
+package com.ramesh;
+
+public class Employee {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Program 1");
+		System.out.println("Program 2");
+	}
+}
---
+++ TestPro/src/com/ramesh/TestMain.java
@@ -0,0 +1,29 @@
+package com.ramesh;
+
+public class TestMain {
+
+	int id;
+	
+	String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+}
